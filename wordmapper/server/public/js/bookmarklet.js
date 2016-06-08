66eb503854bda6091b6d0c4d199ed68859d3a9dd
Arthur Barrett <abarrett@fas.harvard.edu>
2016-06-07 23:17:18 -0400

    Update.

--- wordmapper/server/public/js/bookmarklet.js
+++ wordmapper/server/public/js/bookmarklet.js
@@ -433,6 +433,7 @@
 	Application.prototype.init = function() {
 	  var panel = new Panel();
 	  panel.render();
+	  var sourceTexts = new SourceTexts();
 	};
 
 	//---------------------------------------------------------------------
@@ -461,6 +462,71 @@
 	};
 	Panel.prototype.render = function() {
 	  this.el.html(templates.panel());
+	};
+
+	//---------------------------------------------------------------------
+	var SourceTexts = function() {
+	  this.init();
+	};
+	SourceTexts.prototype.init = function() {
+	  this.onClickWord = this.onClickWord.bind(this);
+	  this.onHoverWord = this.onHoverWord.bind(this);
+	  this.textBoxes = this.selectTextBoxes();
+	  this.wordify();
+	  this.addListeners();
+	};
+	SourceTexts.prototype.addListeners = function() {
+	  
+	};
+	SourceTexts.prototype.onClickWord = function(evt) {
+	  
+	};
+	SourceTexts.prototype.onHoverWord = function(evt) {
+	  
+	};
+	SourceTexts.prototype.selectTextBoxes = function() {
+	  return $(".textboxcontent");
+	};
+	SourceTexts.prototype.wordify = function() {
+	  console.log("wordify");
+	  var _this = this;
+	  this.textBoxes.each(function(i, el) {
+	    console.log(i, el);
+	    if (!_this.isWordified(el)) {
+	      _this.setupWords(el);
+	    }
+	  });
+	};
+	SourceTexts.prototype.isWordified = function(el) {
+	  return $(el).find('.wordmapper-word').length > 0;
+	};
+	SourceTexts.prototype.setupWords = function(el) {
+	  console.log("setupWords");
+	  var node = el;
+	  var content, words, spans;
+	  while (node) {
+	    if (node.nodeType == 1) {
+	      if (node.firstChild) {
+	        node = node.firstChild;
+	      } else if (node.nextSibling) {
+	        node = node.nextSibling;
+	      } else {
+	        node = node.parentNode.nextSibling;
+	      }
+	      console.log("node: ", node.nodeType, node);
+	    } else if (node.nodeType == 3) {
+	      content = node.nodeValue;
+	      words = content.split(/\s+/).filter(function(word) {
+	        return word.length > 0;
+	      });
+	      console.log(words);
+	      if (node.nextSibling) {
+	        node = node.nextSibling;
+	      } else {
+	        node = node.parentNode.nextSibling;
+	      }
+	    }
+	  }
 	};
 
 	//---------------------------------------------------------------------
