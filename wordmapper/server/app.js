4915c5c8a26acf51de366ae18b462d7a2f2bbb50
Arthur Barrett <abarrett@fas.harvard.edu>
2016-06-20 15:19:15 -0400

    Added basic index page for server.

--- wordmapper/server/app.js
+++ wordmapper/server/app.js
@@ -3,14 +3,22 @@ var app = express();
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 8000;
 
+// set the views directory
+app.set('views', __dirname + '/views');
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
 app.use('/static', express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.send('Word Mapper says: Hello World!');
+    // ejs render automatically looks in the views folder
+    res.render('index');
 });
 
 app.listen(port, function () {
-  console.log('Word Mapper listening on port 80!');
+  console.log('Word Mapper listening on port ' + port + '!');
 });
