f11f54a2bee5ede397d712da16a815cbdd84adea
Arthur Barrett <abarrett@fas.harvard.edu>
2016-06-20 14:51:20 -0400

    Fixing server port.

--- wordmapper/server/app.js
+++ wordmapper/server/app.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 80;
+
 app.use('/static', express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.send('Word Mapper says: Hello World!');
 });
 
-app.listen(80, function () {
+app.listen(port, function () {
   console.log('Word Mapper listening on port 80!');
 });
