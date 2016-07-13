8ef465c9dbed46ff630e5f51b34404cdb398b6ad
Arthur Barrett <abarrett@fas.harvard.edu>
2016-07-13 18:46:15 -0400

    Update config.js

--- wordmapper/server/src/config.js
+++ wordmapper/server/src/config.js
@@ -1,19 +1,8 @@
-var pgUrl = require('pg-database-url');
-
-var dbConfig = {
-	host: 'localhost',
-	port: 5432,
-	database: 'wordmapper',
-	username: 'vagrant', // ubuntu
-	password: 'vagrant' // ubuntu
-};
-var dbUrl = pgUrl(dbConfig);
-
 module.exports = {
-	appName: "WordMapper",
+	appName: process.env.APP_NAME || "Word Mapper",
 	port: process.env.PORT || 8000,
 	logLevel: process.env.LOG_LEVEL || 'debug',
-	database: process.env.DATABASE_URL || dbUrl,
+	database: process.env.DATABASE_URL || 'postgres://vagrant:vagrant@localhost:5432/wordmapper',
 	authSecret: process.env.AUTH_SECRET || 'auth.secret',
 	sessionSecret: process.env.SESSION_SECRET || 'session.secret'
 };
