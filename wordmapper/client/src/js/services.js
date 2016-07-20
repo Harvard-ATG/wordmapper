67097ed2c3c1bb9122ab8ba9e33ee0af1722ce51
Arthur Barrett <abarrett@fas.harvard.edu>
2016-07-19 23:32:39 -0400

    Update services.js

--- wordmapper/client/src/js/services.js
+++ wordmapper/client/src/js/services.js
@@ -1,6 +1,6 @@
 module.exports = {
   ImportExportService: require('./services/import_export.js'),
   Persistence: require('./services/persistence.js'),
-	StorageLocal: require('./services/storage_local.js'),
-	StorageRemote: require('./services/storage_remote.js')
+  StorageLocal: require('./services/storage_local.js'),
+  StorageRemote: require('./services/storage_remote.js')
 };
