2bb5993f0491676c8b1e6c30aa171db610a36d2d
Arthur Barrett <abarrett@fas.harvard.edu>
2016-07-21 11:09:48 -0400

    Fixed failing test.

--- wordmapper/client/test/src/services/persistence.spec.js
+++ wordmapper/client/test/src/services/persistence.spec.js
@@ -1,13 +1,21 @@
 var _ = require('lodash');
-var Persistence = require('../../../src/js/services/persistence.js');
+var services = require('../../../src/js/services.js');
+var models = require('../../../src/js/models.js');
+var Settings = require('../../../src/js/settings.js');
+
+var Persistence = services.Persistence;
 
 describe("Persistence service", function() {
   it("should have a constructor", function() {
-    var persistence = new Persistence();
+    var options = {
+      models: {
+        alignments: new models.Alignments(),
+        sources: new models.Sources(),
+        user: new models.User()
+      },
+      settings: new Settings()
+    };
+    var persistence = new Persistence(options);
     expect(persistence instanceof Persistence);
   })
-  it("should reject invalid storage types", function() {
-    var persistence = new Persistence({ stores: ['foobar'] });
-    expect(persistence.stores).toBe([]);
-  });
 });
