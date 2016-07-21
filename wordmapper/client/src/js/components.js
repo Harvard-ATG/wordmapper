86ee41a97abfb13546ab021464a94dc499525f06
Arthur Barrett <abarrett@fas.harvard.edu>
2016-07-21 11:18:00 -0400

    Updated component.js

--- wordmapper/client/src/js/components.js
+++ wordmapper/client/src/js/components.js
@@ -1,3 +1,7 @@
 module.exports = {
-    Application: require('./components/application.js')
+    Application: require('./components/application.js'),
+    Login: require('./components/login.js'),
+    Overlay: require('./components/overlay.js'),
+    Panel: require('./components/panel.js'),
+    Text: require('./components/text.js')
 };
