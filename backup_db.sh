2e1f18e1a02e84c8e3fcc205a089fde299418a66
Arthur Barrett <abarrett@fas.harvard.edu>
2016-09-16 16:29:47 -0400

    Updated backup script.

--- backup_db.sh
+++ backup_db.sh
@@ -1,13 +1,51 @@
 #!/bin/bash
+#
+# In order for this script to work, set the following environment 
+# variables in your shell:
+#
+# 	PGHOST
+# 	PGPORT
+# 	PGDATABASE
+# 	PGUSER
+# 	PGPASSWORD
+# 
+# Usage:
+# 
+# 	$ bash backup-db.sh
+#
 
-D=`date +%Y-%m-%d`
-F="wordmapper-db-$D.psql"
-T="/tmp"
-FF="$T/$F"
-FFG="$FF.gz"
-DB="wordmapper"
-cd $T
-pg_dump $D > $FF
-gzip $FF
-aws glacier upload-archive --account-id -  --vault-name wordmapper --body $FFG
+TODAY=`date +%Y-%m-%d`
+FILE_NAME="wordmapper-db-$TODAY.psql"
+FILE_PATH="/tmp/$FILE_NAME"
+FILE_ARCHIVE="$FILE_PATH.gz"
+DATABASE_URL="postgres://$PGUSER:$PGPASSWORD@$PGHOST:$PGPORT/$PGDATABASE"
+VAULT_NAME="$PGDATABASE"
+ARCHIVE_DESCRIPTION="Wordmapper database backup for $TODAY"
 
+echo "Starting backup process..."
+	
+echo "--> Dumping database [$DATABASE_URL] to [$FILE_PATH]"
+if pg_dump $DATABASE_URL > $FILE_PATH; then
+	echo "...success."
+else
+	echo "...failed. Aborting."
+	exit $?
+fi
+
+echo "--> Creating archive $FILE_ARCHIVE"
+if gzip $FILE_PATH; then
+	echo "...success."
+else
+	echo "...failed. Aborting."
+	exit $?
+fi
+
+echo "--> Uploading archive to AWS Glacier"
+if aws glacier upload-archive --account-id -  --vault-name $VAULT_NAME --archive-description "$ARCHIVE_DESCRIPTION" --body $FILE_ARCHIVE ; then
+	echo "...success."
+else
+	echo "...failed.  Aborting."
+	exit $?
+fi
+
+echo "Backup process complete."
