6ed5310f4d8fe27a5ddf27602434b434926e15e8
Arthur Barrett <abarrett@fas.harvard.edu>
2016-09-19 14:27:13 -0400

    Updated backup script comments and added check for required environment variables.

--- backup_db.sh
+++ backup_db.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 #
+# DESCRIPTION
+# -----------
+#
+# This script performs a backup of the postgres database by doing a 
+# `pg_dump` of the postgres database and uploading the gzipped dump
+# file to an AWS Glacier vault. 
+#
+# SETUP
+# -----
+#
 # In order for this script to work, set the following environment 
 # variables in your shell:
 #
@@ -8,10 +18,17 @@
 # 	PGDATABASE
 # 	PGUSER
 # 	PGPASSWORD
+#
+# Then add a script like the following to /etc/cron.{daily,weekly} to 
+# enable backups on a daily or weekly basis:
+#
+#	#!/bin/bash
+#	/bin/su - ubuntu -s /bin/bash -c "/home/ubuntu/wordmapper/backup_db.sh"
 # 
-# Usage:
+# USAGE
+# -----
 # 
-# 	$ bash backup-db.sh
+# 	$ ./backup-db.sh
 #
 
 TODAY=`date +%Y-%m-%d`
@@ -21,6 +38,11 @@ FILE_ARCHIVE="$FILE_PATH.gz"
 DATABASE_URL="postgres://$PGUSER:$PGPASSWORD@$PGHOST:$PGPORT/$PGDATABASE"
 VAULT_NAME="$PGDATABASE"
 ARCHIVE_DESCRIPTION="Wordmapper database backup for $TODAY"
+
+if [ ! -n "$PGDATABASE" ]; then
+	echo "Missing postgres 'PG' environment variables required to execute backup. Aborting."
+	exit 1
+fi
 
 echo "Starting backup process..."
 	
@@ -33,7 +55,7 @@ else
 fi
 
 echo "--> Creating archive $FILE_ARCHIVE"
-if gzip $FILE_PATH; then
+if /bin/rm -f "$FILE_ARCHIVE" && gzip "$FILE_PATH"; then
 	echo "...success."
 else
 	echo "...failed. Aborting."
